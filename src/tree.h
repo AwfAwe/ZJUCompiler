c7dc50334e6f4dac00413cbc462d3ca4767c03f9
menci725 <97275048+menci725@users.noreply.github.com>
2023-05-30 22:16:18 +0800

    Add files via upload
    update tree.h

--- src/tree.h
+++ src/tree.h
@@ -1,1 +1,39 @@
+#pragma once
+#include "symbolTable.h"
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
 
+struct Attr {
+    Type type;  //类型，只有函数和变量有
+    int numval; //数值
+    int isdone;
+};
+
+class TreeNode {
+  public:
+    TreeNode() {}
+    ~TreeNode() {}
+    TreeNode(string t_value) {
+        type = "GENERATOR";
+        value = t_value;
+        attr.type = Void;
+        attr.numval = 0;
+        attr.isdone = 0;
+    }
+
+    TreeNode *AddNode(TreeNode *node) {
+        child.push_back(node);
+        return this;
+    }
+
+    int row;
+    // int col;
+    string type;              //类型
+    string value;             //字符值
+    struct Attr attr;         //属性
+    vector<TreeNode *> child; //子节点
+};
+
+typedef TreeNode *SyntaxTree;
